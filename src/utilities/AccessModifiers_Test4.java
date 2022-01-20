532c2ef62642d5525f62b69f86f59fbb7ba9d7d8
MadinaR <madinaramazanova0510@gmail.com>
2022-01-20 11:03:16 -0500

    day40_Protected Access Modifier & FinalKeyword

---
+++ src/utilities/AccessModifiers_Test4.java
@@ -0,0 +1,21 @@
+package utilities;
+
+import day40_FinalKeyword.ProtectedAccessModifier;
+
+public class AccessModifiers_Test4 extends ProtectedAccessModifier {
+
+    public static void main(String[] args) {
+
+        // System.out.println(AccessModifiers_Test4.name1); //default is not visible outside the package
+        System.out.println(AccessModifiers_Test4.name2); //protected is visible only in subclass (child class)
+
+       // AccessModifiers_Test4.method1(); // default
+        AccessModifiers_Test4.method2();
+
+
+
+
+    }
+
+
+}
