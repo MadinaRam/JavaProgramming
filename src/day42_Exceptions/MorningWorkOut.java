07edf09651dbeb45119fa7b56d9e50c17adc89e0
MadinaR <madinaramazanova0510@gmail.com>
2022-01-27 10:59:05 -0500

    day42_Exceptions

--- src/day42_Exceptions/MorningWorkOut.java
+++ src/day42_Exceptions/MorningWorkOut.java
@@ -38,3 +38,10 @@ public class MorningWorkOut {
         }
     }
 }
+/*
+MorningWorkOut:
+        1. Do 30 push-ups and try pausing 1.5 seconds in each
+
+
+        2. Do 20 Pull-ups and try pausing 2.5 seconds in each
+ */
