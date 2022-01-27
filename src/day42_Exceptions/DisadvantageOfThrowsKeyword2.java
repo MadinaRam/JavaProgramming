0c3d1c6dc171ea3f4eacbe7f4bfa76637870e727
MadinaR <madinaramazanova0510@gmail.com>
2022-01-27 12:21:34 -0500

    day42_Exceptions / ThrowsKeyword

--- src/day42_Exceptions/DisadvantageOfThrowsKeyword2.java
+++ src/day42_Exceptions/DisadvantageOfThrowsKeyword2.java
@@ -6,28 +6,48 @@ import java.io.FileNotFoundException;
 public class DisadvantageOfThrowsKeyword2 {
 
 
+    public static void main(String[] args) throws  InterruptedException {
+
+        // System.out.println("Hello");
+
+        method1();
+        method2();
+        method3();
+
+
+        String str = null;
+        try {
+            System.out.println(str.charAt(0));
+        }catch (RuntimeException e){
+            e.printStackTrace();
+        }
+
+    }
+
+
     public static void method(){
         try {
             method1();
-        } catch (FileNotFoundException e) {
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
 
-    public static void method1() throws FileNotFoundException {
+    public static void method1() throws InterruptedException {
 
-        new FileInputStream("");
+        //new FileInputStream("");
+        Thread.sleep(3000);
 
     }
 
-    public static void method2() throws FileNotFoundException {
+    public static void method2() throws InterruptedException {
 
         method1();
 
     }
 
 
-    public static void method3() throws FileNotFoundException, InterruptedException {
+    public static void method3() throws  InterruptedException {
 
         method2();
         Thread.sleep(1000);
@@ -35,7 +55,7 @@ public class DisadvantageOfThrowsKeyword2 {
     }
 
 
-    public static void method4() throws FileNotFoundException, InterruptedException {
+    public static void method4() throws InterruptedException {
         method3();
     }
 
